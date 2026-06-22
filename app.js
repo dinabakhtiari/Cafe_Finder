f50207a6d3656c07eddc1d3915d62d9704478407
hcirne <henriquepcirne@gmail.com>
2026-06-22 17:15:18 +0200

    added npm setup and logic to connect to mysql database

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const connection = require("./utils/connection.js");
+
+// start connection to database
+connection.connect((err) => {
+    if (err) {
+        console.log('Error connecting to the database: ' + err.stack);
+        return;
+    };
+
+    console.log('Connected to the database as id ' + connection.threadId);
+});
+
+// start app
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+// close connection to database
+// connection.end((err) => {
+//     if (err) {
+//         console.log('Error closing the database connection: ' + err.stack);
+//         return;
+//     };
+// 
+//     console.log('Database connection closed.');
+// });
