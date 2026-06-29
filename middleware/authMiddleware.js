8c4050ce7bd7fb96285c70d821fface9646d7e23
hcirne <henriquepcirne@gmail.com>
2026-06-29 23:28:54 +0200

    fix: bug in authMiddleware and feat: check if user can access resource

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 const requireLogin = (req, res, next) => {
-  if (req.session.userId) {
-    return res.status(401).send("You must be logged in.");
-  }
-  next();
+    if (!req.session.userId) {
+        return res.status(401).send("You must be logged in.");
+    }
+    next();
 };
 
 module.exports = requireLogin;
