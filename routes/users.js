b1cb65f2a0aefcd35adc1c975dfead700bf232a3
hcirne <henriquepcirne@gmail.com>
2026-06-29 23:02:49 +0200

    feat: added delete user function and get user info for prefilling profile

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,23 @@ const router = express.Router();
 const connection = require("../middleware/connectionDB.js");
 const bcryptjs = require('bcryptjs');
 
+router.get('/:id', (req, res) => {
+    connection.query(
+        "SELECT name, email, bio FROM users WHERE id = ?",
+        [req.params.id],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+            if (result.length === 0) {
+                return res.status(404).send("No user found");
+            }
+            return res.json(result[0]);
+        }
+    );
+});
+
+// Update user info
 router.put('/:id', async (req, res) => {
     const { name, email, password, confirm_password, bio } = req.body;
     const userId = req.params.id;
@@ -45,6 +62,26 @@ router.put('/:id', async (req, res) => {
             }
         );
     }
+});
+
+// Delete user account
+router.delete('/:id', (req, res) => {
+    const userId = req.params.id;
+
+    connection.query(
+        "DELETE FROM users WHERE id = ?",
+        [userId],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+            if (result.affectedRows === 0) {
+                return res.status(404).json({ error: "User not found" });
+            }
+            return res.status(204).send();
+        }
+    )
 });
 
 module.exports = router;
